1ca4a599524a6254bc4e5f4f17f7ddcd7ade0704
Agustin Barrero <agustinbarrero5k@gmail.com>
2022-09-05 21:33:26 -0300

    Agregue el archivo js para las funciones de raquetas, un cuadro nuevo en index que explica como navegar en la pagina, y links funcionales en las imagenes de raquetas.html, donde podes ir a la pagina oficial de cada raqueta si haces click en su imagen, falta hacer que sea responsivo el nuevo cuadro que eesta en el index, y hacer dos archivos mas de js para paletas y accesorios, con sus funciones para hacer lo mismo que en raquetas pero para los otros dos html.

---
+++ js/raquetas.js
@@ -0,0 +1,71 @@
+//Aca voy a hacer las funciones para los links de redireccion a las paginas oficiales de cada producto
+
+//Funciones de las raquetas WILSON
+
+// ProOpen 100
+function linkProOpen(){
+    location.href ="https://wilsonstore.com.ar/catalog/product/view/id/110/s/pro-open-100/#239=197"
+}
+// Blade 98V8RG
+function linkBlade98V8Rg(){
+    location.href ="https://wilsonstore.com.ar/blade-98-18x20-v8-rg#239=196"
+}
+// Blade 98V8
+function linkBlade98V8(){
+    location.href ="https://wilsonstore.com.ar/blade-98-16x19-v8-0#239=196"
+}
+// Clash100ProV2
+function linkClash100ProV2(){
+    location.href ="https://wilsonstore.com.ar/clash-100-pro-v2#239=197"
+}
+// Clash100Pro
+function linkClash100Pro(){
+    location.href ="https://wilsonstore.com.ar/catalog/product/view/id/79/s/clash-100-pro/"
+}
+// Clash100Silver
+function linkClash100Silver(){
+    location.href ="https://wilsonstore.com.ar/clash-100-silver#239=196"
+}
+//rolandGarrosTeam102
+function linkRolandGarrosTeam102(){
+    location.href ="https://wilsonstore.com.ar/roland-garros-team-102#239=196"
+}
+// Pro Staff 97V13
+function linkProStaff97V13(){
+    location.href ="https://wilsonstore.com.ar/pro-staff-97-v13"
+}
+
+//Funciones de las raquetas HEAD
+
+// Attitude Elite
+function linkAttitudeElite(){
+    location.href ="https://www.head.com/es_ES/attitude-elite-234321.html"
+}
+// Attitude Pro
+function linkAttitudePro (){
+    location.href ="https://www.head.com/es_ES/attitude-pro-234311.html"
+}
+// Extreme Pro
+function linkExtremePro(){
+    location.href ="https://www.head.com/es_ES/extreme-pro-88.html"
+}
+// Extreme Tour
+function linkExtremeTour(){
+    location.href ="https://www.head.com/es_ES/extreme-tour-2022-235302.html"
+}
+// Challenge MP
+function linkChallengeMp(){
+    location.href ="https://www.head.com/es_ES/challenge-mp-233912.html"
+}
+// Gravity Tour
+function linkGravityTour(){
+    location.href ="https://www.head.com/en/gravity-tour-51.html"
+}
+// Prestige Pro
+function linkPrestigePro(){
+    location.href ="https://www.head.com/en/prestige-pro-236101.html"
+}
+// Radical Pro
+function linkRadicalPro(){
+    location.href ="https://www.head.com/en/radical-pro-137.html"
+}
