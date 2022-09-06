34d6ccd5c946e10de6526301cc69409a521cf06a
Agustin Barrero <agustinbarrero5k@gmail.com>
2022-09-06 03:40:11 -0300

    Agregue el archivo js para las funciones de las paletas.

---
+++ js/paletas.js
@@ -0,0 +1,67 @@
+//Bela LT
+function belaLt(){
+    location.href ="https://www.wilson.com/es-es/product/bela-lt-padel-wr08920#size=70428&colors=74932"
+}
+//Blade Pro V2
+function bladeProV2(){
+    location.href ="https://www.wilson.com/es-es/product/blade-pro-padel-v2-wr06720#size=70428&colors=74959"
+}
+//Bela Elite
+function belaElite(){
+    location.href ="https://www.wilson.com/es-es/product/bela-elite-padel-wr06560#colors=70961&size=70428"
+}
+//Pro Staff Elite
+function proStaffElite(){
+    location.href ="https://www.wilson.com/es-es/product/pro-staff-elite-wr03280#colors=70452&size=70428"
+}
+//Pro Staff Team Padel
+function proStaffTeamPadel(){
+    location.href ="https://www.wilson.com/es-es/product/pro-staff-team-wr03290#colors=70955&size=70428"
+}
+//Pro Staff LT V2
+function proStaffLtV2(){
+    location.href ="https://www.wilson.com/es-es/product/pro-staff-lt-padel-wr11190#colors=84815&size=70428"
+}
+//Pro Staff Elite V2
+function proStaffEliteV2(){
+    location.href ="https://www.wilson.com/es-es/product/pro-staff-v2-elite-padel-wr11220#colors=84776&size=70428"
+}
+//Cupra Wilson
+function cupraWilson(){
+    location.href ="https://www.wilson.com/es-es/product/cupra-padel-6h2o5o539-laa-wr11280#colors=70461&size=70428"
+}
+
+//PALETAS HEAD
+
+//Head Flash
+function headFlash(){
+    location.href ="https://www.head.com/en/flash-iced-grey-2022-228262.html"
+}
+//Head Flash Pro
+function headFlashPro(){
+    location.href ="https://www.head.com/en/flash-pro-2022-228252.html"
+}
+//Head Gravity Elite
+function headGravityElite(){
+    location.href ="https://www.head.com/en/gravity-elite-2022-228182.html"
+}
+//Head Gravity Pro
+function headGravityPro(){
+    location.href ="https://www.head.com/en/gravity-pro-2022-228162.html"
+}
+//Head Gravity Motion
+function headGravityMotion(){
+    location.href ="https://www.head.com/en/gravity-motion-2022-228172.html"
+}
+//Head Zephyr UL
+function headZephyrUl(){
+    location.href ="https://www.head.com/en/zephyr-ul-2022-228222.html"
+}
+//Head Delta Motion
+function headDeltaMotion(){
+    location.href ="https://www.head.com/en/delta-motion-2022-228112.html"
+}
+//Head Evo Delta
+function headEvoDelta(){
+    location.href ="https://www.head.com/en/evo-delta-2022-228282.html"
+}
