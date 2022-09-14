5b23b3b318f95352ee26b60f1f1df41f705488db
Agustin Barrero <agustinbarrero5k@gmail.com>
2022-09-14 17:30:43 -0300

    Agregue el archivo js de accesorios con sus funciones y ahora sepuede ir a la pagina oficial de cada producto del html accesorios

---
+++ js/accesorios.js
@@ -0,0 +1,57 @@
+//Funciones para los accesorios 
+//Funciones accesorios WILSON   
+function adrenaline130Reel(){
+    location.href = "https://wilsonstore.com.ar/adrenaline-130"
+}
+function luxilon4GSoft17Reel(){
+    location.href = "https://wilsonstore.com.ar/luxilon-4g-soft-17-reel-200m"
+}
+function usOpenRedTournament(){
+    location.href = "https://wilsonstore.com.ar/us-open-red-tournament-3-ball-can"
+}
+function usOpenXd(){
+    location.href = "https://wilsonstore.com.pe/tubo-us-open-xd-4-ball"
+}
+function fedTeamCollectionBag(){
+    location.href = "https://wilsonstore.com.ar/fed-team-collection-bag"
+}
+function clashSmallDuffel(){
+    location.href = "https://wilsonstore.com.ar/catalog/product/view/id/111/s/clash-small-duffel/"
+}
+function classicSpongeGrip(){
+    location.href = "https://wilsonstore.com.ar/classic-sponge-grip"
+}
+function proOgSensationOvergrip(){
+    location.href = "https://wilsonstore.com.ar/pro-og-sensation"
+}
+function sublimeGrip(){
+    location.href = "https://wilsonstore.com.ar/sublime-grip1#color=66"
+}
+//Ahora las funciones de los accesorios HEAD  
+function headTourXtSingleCan(){
+    location.href = "https://www.head.com/es_ES/3b-head-tour-xt-single-can.html"
+}
+function headPro4BallSingleCan(){
+    location.href ="https://www.head.com/en/head-pro-4-ball-2.html"
+}
+function ballBasketWithSeparator(){
+    location.href = "https://www.head.com/en/ball-basket-with-separator.html"
+}
+function wristbandRed(){
+    location.href = "https://www.head.com/en/wristband-2-5-25.html"
+}
+function wristband5Orange(){
+    location.href = "https://www.head.com/en/wristband-5-25.html"
+}
+function leatherTourBrown(){
+    location.href ="https://www.head.com/en/leather-tour-1.html"
+}
+function ultimateBlackGrip(){
+    location.href = "https://www.head.com/en/ultimate-6.html"
+}
+function lynxTourReelChampagne(){
+    location.href = "https://www.head.com/en/lynx-tour-reel-4.html"
+}
+function hawkTouchReelAnthracite(){
+    location.href = "https://www.head.com/en/hawk-touch-reel-3.html"
+}
